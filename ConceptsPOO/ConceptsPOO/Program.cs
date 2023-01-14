0ad0170e9d060ae7dd895df799b00468d375f3c0
Galeano_Gustavo <ggaleanoparedes@gmail.com>
2023-01-14 20:02:48 -0300

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts!");
+Console.WriteLine("=============");
+
+
+//Date date1  = new Date(2023, 2, 11);
+
+try
+{
+	Console.WriteLine(new Date(2012, 2, 29));
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(1985, 11, 31));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
+
+
